e80fe00ff5c9c88103ae2e6b0c1d716f743ea9af
zura <makaradze98.s@gmail.com>
2025-07-20 21:42:54 -0400

    s

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,9 @@ export default function Home() {
           className="dark:invert"
           src="/next.svg"
           alt="Next.js logo"
+
+
+          
           width={180}
           height={38}
           priority
